e20c1f392c2bb5275b59b0a50a9bc0b4d8af2a35
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-03-17 23:19:17 +0300

    perform addition $subtraction

--- functions.dart
+++ functions.dart
@@ -1,4 +1,16 @@
-void main(){
-  print ("hello");
-  
+
+
+
+int add(int x, int y) {
+  return x + y;
+}
+
+int subtract(int x, int y) {
+  return x - y;
+}
+void main() {
+  int a = 10;
+  int b = 50;
+  print("Addition : ${add(a, b)}");
+  print("Subtraction : ${subtract(a, b)}");
 }
